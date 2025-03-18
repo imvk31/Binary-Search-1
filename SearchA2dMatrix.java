7fd4ff22df02cb883ecd4c7e291f28eaad9efbb7
Varam <prasadkurra31@gmail.com>
2025-03-18 18:44:13 -0500

    Corrected code.

--- SearchA2dMatrix.java
+++ SearchA2dMatrix.java
@@ -14,8 +14,20 @@ You must write a solution in O(log(m * n)) time complexity.
   Iterate across all the elemets in each row and column to find the target.
   */
 
-  public class SearchA2dMatrix(){
-    public static void main(String args[]){
-        System.out.println("Hellooooo");
+  class SearchA2dMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row =0;
+        int column = matrix.length * matrix[0].length -1;
+        while(row <= column){
+            int pivotIdx = (row + column)/2;
+            int pivotElement = matrix[pivotIdx/matrix[0].length][pivotIdx % matrix[0].length];
+            if(pivotElement == target) 
+            return true;
+            else{
+                if (target < pivotElement) column = pivotIdx - 1;
+                else row = pivotIdx + 1;
+            }
+        }
+        return false;
     }
-  }
+}
--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -1,31 +1,27 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int low =0;
-        int high = nums.length -1;
+    public boolean search(int[][] nums, int target) {
+        int m = nums.length;
+        int n = nums[0].length;
+        int low = 0;
+        int high = m * n - 1;
         while(low <= high){
-         int mid = low+(high-low)/2;
-         if(nums[mid] == target){
-            return mid;
-         }
-         else if(nums[low] <= nums[mid]){
-            if(nums[low] <= target && nums[mid] >= target){
-                high = mid-1;
-            } 
-            else
-            {
+            int mid = low +(high - low)/2;
+            if(nums[mid/n][mid%n] == target) //
+                return true;
+            else if(nums[mid/n][mid%n] < target){
                 low = mid+1;
             }
-         }
-        else{
-            if(nums[high] >= target && nums[mid] <= target){
-                low = mid+1;
-            } 
             else
-            {
-                high = mid-1;
-            }
-         }
+                high = mid -1;
         }
-        return -1;
+        return false;
+    }
+
+    public static void main(String args[]){
+        Solution solution = new Solution();
+        int[][] myMap = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        int target = 17;
+        boolean result = solution.search(myMap, target);
+        System.out.println(result);
     }
 }

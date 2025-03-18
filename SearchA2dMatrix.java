4aeb3aa784fc530647f01932d139eb0399f8ce22
Varam <prasadkurra31@gmail.com>
2025-03-17 21:23:34 -0500

    inary-Search-1

---
+++ SearchA2dMatrix.java
@@ -0,0 +1,21 @@
+/**
+ * You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+ */
+
+ /**
+  * Brute Force Approach
+  Iterate across all the elemets in each row and column to find the target.
+  */
+
+  public class SearchA2dMatrix(){
+    public static void main(String args[]){
+        System.out.println("Hellooooo");
+    }
+  }
